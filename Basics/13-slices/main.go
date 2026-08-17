550b367797edda67604ed20d41021eb638467a8b
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-17 13:25:46 +0500

    slices literals

---
+++ Basics/13-slices/main.go
@@ -0,0 +1,17 @@
+package main
+import "fmt"
+
+func main (){
+
+	//dynamic
+	name := [] string {"Huzaifa", "Ghufran", "Hassan"}
+
+	fmt.Println(name)
+
+	//append values in slice
+
+	var age [] int
+	age = append(age, 22,23,24)
+
+	fmt.Println(age)
+}
