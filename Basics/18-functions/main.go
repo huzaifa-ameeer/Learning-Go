a9cca4ebde98839a593594be761c55f9ecad04fe
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-19 19:56:25 +0500

    variadic functions

--- Basics/18-functions/main.go
+++ Basics/18-functions/main.go
@@ -18,16 +18,34 @@
 // 	fmt.Println("Sum:", sum, "Results: ",result1,",", result2)
 // }
 
-package main
+// package main
+// import "fmt"
+
+// func addAndSub (a int, b int) (sum int, sub int){
+// 	sum = a+b
+// 	sub = a-b
+// 	return
+// }
+
+// func main (){
+// 	r1, r2 := addAndSub(2,3)
+// 	fmt.Println("Sum:", r1, "Sub:", r2)
+// }
+
+package main 
 import "fmt"
 
-func addAndSub (a int, b int) (sum int, sub int){
-	sum = a+b
-	sub = a-b
-	return
+func sumAll (nums... int) int {
+	total := 0
+	for _, value := range nums{
+		total = total + value
+	}
+	return total
 }
 
 func main (){
-	r1, r2 := addAndSub(2,3)
-	fmt.Println("Sum:", r1, "Sub:", r2)
+	// fmt.Println(sumAll(1,2,3,4,5,6,7,8,9,10))
+
+	values := [] int {1,2,3,4,5}
+	fmt.Println(sumAll(values...))
 }
