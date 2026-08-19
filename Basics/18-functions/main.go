a7f17f08145fde2cf3e4548e96a64f614bae6304
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-08-19 19:52:26 +0500

    named returned values

--- Basics/18-functions/main.go
+++ Basics/18-functions/main.go
@@ -1,19 +1,33 @@
+// package main
+// import "fmt"
+
+// func add (a int, b int) int {
+// 	return a+b
+// }
+
+// func addAndProduct (a int, b int) (int, int) {
+// 	sum := a+b
+// 	product := a*b
+// 	return sum, product
+// }
+
+// func main (){
+// 	sum := add(2,4)
+// 	// fmt.Println(sum)
+// 	result1, result2 := addAndProduct(3,4)
+// 	fmt.Println("Sum:", sum, "Results: ",result1,",", result2)
+// }
+
 package main
 import "fmt"
 
-func add (a int, b int) int {
-	return a+b
-}
-
-func addAndProduct (a int, b int) (int, int) {
-	sum := a+b
-	product := a*b
-	return sum, product
+func addAndSub (a int, b int) (sum int, sub int){
+	sum = a+b
+	sub = a-b
+	return
 }
 
 func main (){
-	sum := add(2,4)
-	// fmt.Println(sum)
-	result1, result2 := addAndProduct(3,4)
-	fmt.Println("Sum:", sum, "Results: ",result1,",", result2)
+	r1, r2 := addAndSub(2,3)
+	fmt.Println("Sum:", r1, "Sub:", r2)
 }
